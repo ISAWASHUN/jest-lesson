6670adfb589041e2331c92738324bac04e76df4e
ISAWASHUN <syun20000616@icloud.com>
2024-08-16 18:10:04 +0900

    パラメタライズテスト

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,5 +1,14 @@
 import { sum } from "./sum";
 
-it("1と2を足すと3になる", () => {
-  expect(sum(1, 2)).toBe(3);
+// it("1と2を足すと3になる", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+
+it.each([
+  [1, 2, 3],
+  [2, 3, 5],
+  [3, 4, 7],
+])("sum(%i, %i) = %i", (a, b, expected) => {
+  expect(sum(a, b)).toBe(expected);
 });
